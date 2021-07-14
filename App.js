0728d8ddba795a034e4151a5aac5080546902aea
Paul Ezaga <paulezaga@gmail.com>
2021-07-14 12:29:16 +0200

    Bottom Tab Navigation

--- App.js
+++ App.js
@@ -1,21 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet } from 'react-native'
+import { createBottomTabNavigator, CreateAppContainer } from 'react-navigation-tabs'
+import { PeopleScreen } from './screens/PeopleScreen'
+import { DecisionScreen } from './screens/DecisionScreen'
+import { RestaurantsScreen } from './screens/RestaurantsScreen'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import { createAppContainer } from 'react-navigation';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const platformOS = Platform.OS.toLowerCase()
+let styles = {};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const tabs = createBottomTabNavigator({
+  PeopleScreen: {
+    screen: PeopleScreen,
+    navigationOptions: {
+      tabBarLabel: "People",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="user" color={tintColor} size={32} style={styles.marginTop} />
+      )
+    }
+  },  
+  DecisionScreen:{
+    screen: DecisionScreen,
+    navigationOptions: {
+      tabBarLabel: "Decision",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="hourglass" color={tintColor} size={32} />
+      )
+    }
   },
-});
+  RestaurantsScreen: {
+    screen: RestaurantsScreen,
+    navigationOptions: {
+      tabBarLabel: "Restaurants",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="university" color={tintColor} size={32} />
+      )
+    }
+  }
+})
+
+styles = StyleSheet.create({
+  iconStyle: {
+    marginTop: 15
+  }
+})
+
+const App = createAppContainer(tabs)
+
+export default App
