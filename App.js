2325a4b4e5cf2f2e61de5167cfe5ee8b6301919b
Paul Ezaga <paulezaga@gmail.com>
2021-07-15 11:19:32 +0200

    React Navigation Version 4

--- App.js
+++ App.js
@@ -1,51 +1,31 @@
 import React from 'react';
-import { Platform, StyleSheet } from 'react-native'
-import { createBottomTabNavigator, CreateAppContainer } from 'react-navigation-tabs'
+import 'react-native-gesture-handler';
+import { View, Text } from 'react-native'
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 import { PeopleScreen } from './screens/PeopleScreen'
 import { DecisionScreen } from './screens/DecisionScreen'
 import { RestaurantsScreen } from './screens/RestaurantsScreen'
-import Icon from 'react-native-vector-icons/FontAwesome'
-import { createAppContainer } from 'react-navigation';
 
-const platformOS = Platform.OS.toLowerCase()
-let styles = {};
 
-const tabs = createBottomTabNavigator({
-  PeopleScreen: {
-    screen: PeopleScreen,
-    navigationOptions: {
-      tabBarLabel: "People",
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="user" color={tintColor} size={32} style={styles.marginTop} />
-      )
-    }
-  },  
-  DecisionScreen:{
-    screen: DecisionScreen,
-    navigationOptions: {
-      tabBarLabel: "Decision",
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="hourglass" color={tintColor} size={32} />
-      )
-    }
+
+const AppNavigator = createStackNavigator(
+  { People: PeopleScreen, 
+    Decision: DecisionScreen, 
+    Restaurants: RestaurantsScreen 
   },
-  RestaurantsScreen: {
-    screen: RestaurantsScreen,
-    navigationOptions: {
-      tabBarLabel: "Restaurants",
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="university" color={tintColor} size={32} />
-      )
-    }
-  }
-})
+  { 
+    initialRouteName: 'Restaurants',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }    
+);
 
-styles = StyleSheet.create({
-  iconStyle: {
-    marginTop: 15
-  }
-})
-
-const App = createAppContainer(tabs)
-
-export default App
+export default createAppContainer(AppNavigator);
