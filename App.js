c369f1b63242a8a596dcdf17fe7c67cbd83a1401
Paul Ezaga <paulezaga@gmail.com>
2021-07-15 17:48:17 +0200

    Tabs Combined with Screens

--- App.js
+++ App.js
@@ -1,27 +1,55 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { PeopleScreen } from './screens/PeopleScreen'
 import { DecisionScreen } from './screens/DecisionScreen'
 import { RestaurantsScreen } from './screens/RestaurantsScreen'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-function HomeScreen() {
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+const Tab1 = () => (<Text> Tab 1 </Text>)
+const Tab2 = () => (<Text> Tab 2 </Text>)
+const Tab3 = () => (<Text> Tab 3 </Text>)
+
+const MyTabs = () => {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
+      <Tab.Navigator>
+        <Tab.Screen name="Tab1" component={Tab1} />
+        <Tab.Screen name="Tab2" component={Tab2} />
+        <Tab.Screen name="Tab3" component={Tab3} />
+      </Tab.Navigator>    
+  )
 }
 
-const Stack = createStackNavigator();
+const screenOptions = {
+  headerStyle: {
+    backgroundColor: '#f4511e',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+};
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Decision" >
-        <Stack.Screen name="People" component={PeopleScreen} />
-        <Stack.Screen name="Decision" component={DecisionScreen} />
+      <Stack.Navigator initialRouteName="HomeTab" screenOptions={screenOptions}>
+        <Stack.Screen name="HomeTab" component={MyTabs} />
+        <Stack.Screen name="People" component={PeopleScreen} options={{ title: 'My People' }} />
+        <Stack.Screen name="Decision" component={DecisionScreen} 
+            options={{          
+              headerRight: () => (
+                <Button
+                  onPress={() => alert('This is a button!')}
+                  title="Info"                  
+                />
+              ),
+        }} />
         <Stack.Screen name="Restaurant" component={RestaurantsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
--- screens/PeopleScreen.js
+++ screens/PeopleScreen.js
@@ -4,9 +4,11 @@ import { NavigationContainer } from '@react-navigation/native';
 
 
 export function PeopleScreen({ navigation }) {
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>People Screen</Text>
+      
       <Button
         title="Go to Restaurant"
         onPress={() => navigation.navigate("Restaurant")}
