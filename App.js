8e946764cffcb87f63ab440146e63bb2475e8cfd
Paul Ezaga <paulezaga@gmail.com>
2021-07-16 18:22:27 +0200

    Tabs within Tab Navigation

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-function HomeScreen({ navigation }) {
+function Home({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Button
@@ -20,23 +20,65 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function NotificationsScreen({ navigation }) {
+function Feed({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
+      <Button onPress={() => navigation.navigate("Home")} title="Profile" />
     </View>
   );
 }
 
-const Drawer = createDrawerNavigator();
+function Notifications({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.navigate("FeedGI")} title="Go back home" />
+    </View>
+  );
+}
+
+function Profile({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Settings')}
+        title="Go to Settings"
+      />
+    </View>
+  );
+}
+
+function Settings({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.navigate("Profile")} title="Profile" />
+    </View>
+  );
+}
+
+
+
+
+const Tab = createBottomTabNavigator();
+const Stack = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="Notifications" component={Notifications} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeTabs} />
+        <Stack.Screen name="Profile" component={Profile} />
+        <Stack.Screen name="Settings" component={Settings} />
+      </Stack.Navigator>     
     </NavigationContainer>
   );
 }
