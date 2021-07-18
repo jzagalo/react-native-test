88980a68ef643f4c26aed0e864cfaaee7d3780d0
Paul Ezaga <paulezaga@gmail.com>
2021-07-18 14:48:16 +0200

    Dynamic Tabs Title from for Tabs inserted into Screen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { View, Text, Button, StatusBar, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, getFocusedRouteNameFromRoute } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { PeopleScreen } from './screens/PeopleScreen'
 import { DecisionScreen } from './screens/DecisionScreen'
@@ -11,31 +11,74 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import SafeAreaView from 'react-native-safe-area-view';
 
-function Demo() {
+function getHeaderTitle(route) {
+  // If the focused route is not found, we need to assume it's the initial screen
+  // This can happen during if there hasn't been any navigation inside the screen
+  // In our case, it's "Feed" as that's the first screen inside the navigator
+  const routeName = getFocusedRouteNameFromRoute(route) ?? 'Feed';
+
+  switch (routeName) {
+    case 'Feed':
+      return 'News feed';
+    case 'Profile':
+      return 'My profile';
+    case 'Account':
+      return 'My account';
+  }
+}
+
+function FeedScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center' }}>
-      <Text>This is top text.</Text>
-      <Text>This is bottom text.</Text>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to Settings"
+        onPress={() => navigation.navigate('Settings')}
+      />
     </View>
   );
 }
-const Stack = createStackNavigator();
+
+function ProfileScreen() {
+  return <View />;
+}
+
+function AccountScreen() {
+  return <View />;
+}
+
+function SettingsScreen() {
+  return <View />;
+}
+
 const Tab = createBottomTabNavigator();
+
+function HomeTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Feed" component={FeedScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen name="Account" component={AccountScreen} />
+    </Tab.Navigator>
+  );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home">
-          {() => (
-            <Tab.Navigator initialRouteName="Analytics" >
-              <Tab.Screen name="Analytics" component={Demo} />
-              <Tab.Screen name="Profile" component={Demo} />
-            </Tab.Navigator>
-          )}
-        </Stack.Screen>
-        <Stack.Screen name="Settings" component={Demo} />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeTabs}
+          options={({ route }) => ({
+            headerTitle: getHeaderTitle(route),
+          })}
+        />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+
